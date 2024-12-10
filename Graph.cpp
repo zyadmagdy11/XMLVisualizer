bb3631d8051d32cf523cda1b26c80e658fe92737
MostafaHamada <167246629+7ksha@users.noreply.github.com>
2024-12-10 10:32:59 +0200

    Part of Mostafa finished

--- Graph.cpp
+++ Graph.cpp
@@ -5,43 +5,47 @@
 
 using namespace std;
 
-struct User {
+struct User
+{
     string id;
     string name;
     vector<string> posts;
     vector<string> Followers_id;
 };
 // Weighted, Directed Graph
-class Graph {
+class Graph
+{
 private:
     int numVer;
     int maxVer;
-    User* vertices;          
-    int** edges;             
-    bool* marks;             
-    
+    User *vertices;
+    int **edges;
+    bool *marks;
+
 public:
-    Graph(int max) 
+    Graph(int max)
     {
         numVer = 0;
         maxVer = max;
-        vertices = new User[max]; 
-        edges = new int*[max];     
-        for (int i = 0; i < max; i++) 
+        vertices = new User[max];
+        edges = new int *[max];
+        for (int i = 0; i < max; i++)
         {
             edges[i] = new int[max];
-            for (int j = 0; j < max; j++) 
+            for (int j = 0; j < max; j++)
             {
-                edges[i][j] = 0;  
+                edges[i][j] = 0;
             }
         }
-        marks = new bool[max];    
-        for (int i = 0; i < max; i++) marks[i] = false;
+        marks = new bool[max];
+        for (int i = 0; i < max; i++)
+            marks[i] = false;
     }
 
-    ~Graph() {
+    ~Graph()
+    {
         delete[] vertices;
-        for (int i = 0; i < maxVer; i++) 
+        for (int i = 0; i < maxVer; i++)
         {
             delete[] edges[i];
         }
@@ -49,54 +53,61 @@ public:
         delete[] marks;
     }
 
-    void AddVertex(User vertex) 
+    void AddVertex(User vertex)
     {
-        if (numVer < maxVer) {
+        if (numVer < maxVer)
+        {
             vertices[numVer] = vertex;
             numVer++;
-        } else {
+        }
+        else
+        {
             cout << "Max vertices reached, cannot add more users." << endl;
         }
     }
 
-    void AddEdge(User fromVertex, User toVertex, int weight) 
+    void AddEdge(User fromVertex, User toVertex, int weight)
     {
-        int row = indexOf(fromVertex);  
+        int row = indexOf(fromVertex);
         int col = indexOf(toVertex);
-        if (row != -1 && col != -1) {
+        if (row != -1 && col != -1)
+        {
             edges[row][col] = weight;
-        } 
-        else 
+        }
+        else
         {
             cout << "Invalid user IDs for edge." << endl;
         }
     }
-    void clearMarks() 
+    void clearMarks()
     {
-        for (int i = 0; i < numVer; i++)  marks[i] = false; 
+        for (int i = 0; i < numVer; i++)
+            marks[i] = false;
     }
 
-    int indexOf(User vertex) 
+    int indexOf(User vertex)
     {
-        for (int i = 0; i < numVer; i++) 
+        for (int i = 0; i < numVer; i++)
         {
-            if (vertices[i].id == vertex.id) return i;  // Compare by ID
+            if (vertices[i].id == vertex.id)
+                return i; // Compare by ID
         }
         return -1;
     }
-    int indexOf(string id) 
+    int indexOf(string id)
     {
-        for (int i = 0; i < numVer; i++) 
+        for (int i = 0; i < numVer; i++)
         {
-            if (vertices[i].id == id) return i;  // Compare by ID
+            if (vertices[i].id == id)
+                return i; // Compare by ID
         }
         return -1;
     }
 
-    void parseXML(string filename) 
+    void parseXML(string filename)
     {
         ifstream file(filename);
-        if (!file.is_open()) 
+        if (!file.is_open())
         {
             cout << "Failed to open file.\n";
             return;
@@ -106,155 +117,211 @@ public:
         string currentId, currentName;
         vector<string> currentPosts;
         vector<string> currentFollowers;
-        bool readingPosts = false, readingFollowers = false, readingID = false;      //flags to indicate start of nested tag
+        bool readingPosts = false, readingFollowers = false, readingID = false; // flags to indicate start of nested tag
 
-        while (getline(file, line)) 
+        while (getline(file, line))
         {
-            if (line.empty()) continue;
+            if (line.empty())
+                continue;
 
-            if (line.find("<user>") != string::npos) 
+            if (line.find("<user>") != string::npos)
             {
                 currentPosts.clear();
                 currentFollowers.clear();
                 readingID = true;
             }
 
-            if (readingID && line.find("<id>") != string::npos) 
+            if (readingID && line.find("<id>") != string::npos)
             {
                 currentId = line.substr(line.find("<id>") + 4, line.find("</id>") - line.find("<id>") - 4);
                 readingID = false;
             }
-            if (line.find("</id>") != string::npos) 
+            if (line.find("</id>") != string::npos)
             {
                 readingID = false;
             }
 
-            if (line.find("<name>") != string::npos) 
+            if (line.find("<name>") != string::npos)
             {
                 currentName = line.substr(line.find("<name>") + 6, line.find("</name>") - line.find("<name>") - 6);
             }
 
-            if (line.find("<posts>") != string::npos) 
+            if (line.find("<posts>") != string::npos)
             {
                 readingPosts = true;
             }
 
-            if (readingPosts && line.find("<post>") != string::npos) 
+            if (readingPosts && line.find("<post>") != string::npos)
             {
                 string post = line.substr(line.find("<post>") + 6, line.find("</post>") - line.find("<post>") - 6);
                 currentPosts.push_back(post);
             }
 
-            if (line.find("</posts>") != string::npos) 
+            if (line.find("</posts>") != string::npos)
             {
                 readingPosts = false;
             }
 
-            if (line.find("<followers>") != string::npos) 
+            if (line.find("<followers>") != string::npos)
             {
                 readingFollowers = true;
             }
 
-            if (readingFollowers && line.find("<id>") != string::npos) 
+            if (readingFollowers && line.find("<id>") != string::npos)
             {
                 string followerId = line.substr(line.find("<id>") + 4, line.find("</id>") - line.find("<id>") - 4);
                 currentFollowers.push_back(followerId);
             }
 
-            if (line.find("</followers>") != string::npos) 
+            if (line.find("</followers>") != string::npos)
             {
                 readingFollowers = false;
             }
 
-            if (line.find("</user>") != string::npos) 
+            if (line.find("</user>") != string::npos)
             {
                 User newUser = {currentId, currentName, currentPosts, currentFollowers};
                 AddVertex(newUser);
             }
         }
         file.close();
-        addEdgesBetweenUsers();  
+        addEdgesBetweenUsers();
     }
 
-    void addEdgesBetweenUsers() 
+    void addEdgesBetweenUsers()
     {
-        for (int i = 0; i < numVer; i++) 
+        for (int i = 0; i < numVer; i++)
         {
             User currentUser = vertices[i];
-            for (string followerId : currentUser.Followers_id) 
+            for (string followerId : currentUser.Followers_id)
             {
-                int followerIndex = indexOf(followerId); 
-                if (followerIndex != -1) 
+                int followerIndex = indexOf(followerId);
+                if (followerIndex != -1)
                 {
                     User newFollower = vertices[followerIndex];
-                    AddEdge(newFollower, currentUser, 1);  
-                } 
-                else 
+                    AddEdge(newFollower, currentUser, 1);
+                }
+                else
                 {
                     // Create Unknown User if follower is not found
                     User newFollower = {followerId, "Unknown User", vector<string>(), vector<string>()};
-                    AddVertex(newFollower);  
+                    AddVertex(newFollower);
                     AddEdge(newFollower, currentUser, 1);
                 }
             }
         }
     }
 
-    void display() 
+    User most_influencer()
     {
-        for (int i = 0; i < numVer; i++) 
+        int maxFollowers = -1;
+        User mostInfluentialUser;
+        // Traverse each user and count the followers
+        for (int i = 0; i < numVer; i++)
+        {
+            int followerCount = vertices[i].Followers_id.size();
+            if (followerCount > maxFollowers)
+            {
+                maxFollowers = followerCount;
+                mostInfluentialUser = vertices[i];
+            }
+        }
+        return mostInfluentialUser;
+    }
+
+    vector<string> searchPosts(string searchTerm)
+    {
+        vector<string> matchedPosts;
+        for (int i = 0; i < numVer; i++)
+        {
+            for (string post : vertices[i].posts)
+            {
+                if (post.find(searchTerm) != string::npos)
+                {
+                    matchedPosts.push_back("User: " + vertices[i].name + " (ID: " + vertices[i].id + ") - " + post);
+                }
+            }
+        }
+        return matchedPosts;
+    }
+
+    void display()
+    {
+        for (int i = 0; i < numVer; i++)
         {
             cout << "User Name: " << vertices[i].name << endl;
             cout << "User ID  : " << vertices[i].id << endl;
             cout << "Posts: \n";
-            for (string post : vertices[i].posts) 
+            for (string post : vertices[i].posts)
             {
                 cout << post << "\n";
             }
-
             cout << "\nFollowers: ";
-            for (int j = 0; j < numVer; j++) 
+            for (int j = 0; j < numVer; j++)
             {
-                if (edges[j][i] != 0) 
+                if (edges[j][i] != 0)
                 {
-                    cout << vertices[j].name<< ", ";
+                    cout << vertices[j].name << ", ";
                 }
             }
             cout << "\n======================================\n";
         }
     }
 };
-User most_active() 
+User most_active()
 {
-    int maxActivity = -1; 
+    int maxActivity = -1;
     User mostActiveUser;
 
-    for (int i = 0; i < numVer; i++) 
+    for (int i = 0; i < numVer; i++)
     {
         int activityScore = 0;
 
-        for (int j = 0; j < numVer; j++) 
+        for (int j = 0; j < numVer; j++)
         {
-            if (edges[i][j] != 0) 
+            if (edges[i][j] != 0)
             {
-                activityScore++;  
+                activityScore++;
             }
         }
 
-        if (activityScore > maxActivity) 
+        if (activityScore > maxActivity)
         {
             maxActivity = activityScore;
-            mostActiveUser = vertices[i];  
+            mostActiveUser = vertices[i];
         }
     }
 
-    return mostActiveUser; 
+    return mostActiveUser;
 }
 
-int main() 
+int main()
 {
     Graph Network(30);
     Network.parseXML("Network.xml");
     Network.display();
+    return 0;
+
+    User mostInfluential = Network.most_influencer();
+    cout << "Most Influential User: " << mostInfluential.name << "  ID: " << mostInfluential.id << endl;
+
+    cout << "=====================================\n"
+         << endl;
+
+
+
+
+
+
+
+    string searchTerm = "Charlie";
+    vector<string> posts = Network.searchPosts(searchTerm);
+
+    cout << "Posts containing '" << searchTerm << "':" << endl;
+    for (const string &post : posts)
+    {
+        cout << post << endl;
+    }
+
     return 0;
 }
